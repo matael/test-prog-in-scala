b6c1339747e55d5668df3e9aa91d49db5186e537
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-07-10 15:20:45 +0200

    Tests pass

--- src/main/scala/ChainedAdd.scala
+++ src/main/scala/ChainedAdd.scala
@@ -4,10 +4,11 @@ import expr._
 object ChainSimplifier {
 
   def simplify(expr: Expr): Expr = expr match {
-    case BinOp("+", x, BinOp("+", y, _)) if x == y =>
-      BinOp("*", Number(2), simplify(x))
-    case BinOp("+", x, y) if x == y => BinOp("*", Number(2), simplify(x))
-    case BinOp("*", Number(a), BinOp("*", Number(b), e)) => BinOp("*", Number(a*b), e)
+    case BinOp("+", x, BinOp("+", y, e)) if x == y =>
+      simplify(BinOp("+", BinOp("*", Number(2), simplify(x)), simplify(e)))
+    case BinOp("+", x, y) if x == y => simplify(BinOp("*", Number(2), simplify(x)))
+    case BinOp("*", Number(a), BinOp("*", Number(b), e)) =>
+      simplify(BinOp("*", Number(a*b), simplify(e)))
     case _ => expr
   }
 }
--- src/test/scala/ChainedAddTest.scala
+++ src/test/scala/ChainedAddTest.scala
@@ -3,6 +3,22 @@ import expr._
 import expr.cs._
 
 class ChainSuite extends FunSuite {
+  test("x+x should become 2x") {
+    val x = Var("x")
+    assert(BinOp("*", Number(2), x) == ChainSimplifier.simplify(BinOp("+", x, x)))
+  }
+  test("x+x+3 should become 2x+3") {
+    val x = Var("x")
+    val ex = BinOp("+", BinOp("*", Number(2), x), Number(3))
+    val init =
+      BinOp("+", x,
+        BinOp("+", x, Number(3))
+      )
+
+    val res = ChainSimplifier.simplify(init)
+    assert(ex == res)
+  }
+
   test("x+x+x+x should become 4x") {
     val x = Var("x")
     val ex = BinOp("*", Number(4), x)
@@ -14,7 +30,7 @@ class ChainSuite extends FunSuite {
       )
     val res = ChainSimplifier.simplify(init)
 
-    assert(ex == res)
+    assert(ex === res)
   }
 }
 
